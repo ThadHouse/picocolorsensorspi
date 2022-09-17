1e0cc4f8d11f95caee38517b64bd78e9b938fe5d
Thad House <thadhouse1@gmail.com>
2022-09-16 19:27:46 -0700

    Move Stretched SPI to its own class

--- src/picocolorsensorspi.c
+++ src/picocolorsensorspi.c
@@ -7,166 +7,18 @@
 #include "hardware/sync.h"
 #include "pico/multicore.h"
 #include "stretched_spi.pio.h"
+#include "stretched_spi.h"
 #include "SEGGER_RTT.h"
+#include <stdint.h>
 
 // SPI Defines
-// Any pins can be used, but SCK must be 1 after MOSI
+// Any pins can be used, but SCK must be 1 after COPI
 #define PIN_CS   1
 #define PIN_SCK  3
-#define PIN_MOSI 2
-#define PIN_MISO 4
+#define PIN_COPI 2
+#define PIN_CIPO 4
 
 #define PIN_DBG  15
-
-static void setup_write_sm(PIO pio, uint *sm, uint* offset) {
-    *offset = pio_add_program(pio, &spi_write_loop_program);
-    *sm = pio_claim_unused_sm(pio, true);
-    pio_sm_config c = spi_write_loop_program_get_default_config(*offset);
-    sm_config_set_in_pins(&c, PIN_MOSI);
-    sm_config_set_out_pins(&c, PIN_MISO, 1);
-    pio_gpio_init(pio, PIN_MISO);
-    pio_sm_set_consecutive_pindirs(pio, *sm, PIN_MISO, 1, true);
-
-    sm_config_set_out_shift(
-        &c,
-        false, // Shift-to-right = false (i.e. shift to left)
-        true,  // Autopush enabled
-        8      // Autopush threshold = 8
-    );
-    pio_sm_init(pio, *sm, *offset, &c);
-}
-
-static void setup_read_sm(PIO pio, uint *sm, uint* offset, irq_handler_t prepare_data_handler) {
-    *offset = pio_add_program(pio, &spi_loop_program);
-    *sm = pio_claim_unused_sm(pio, true);
-    pio_sm_config c = spi_loop_program_get_default_config(*offset);
-    sm_config_set_in_pins(&c, PIN_MOSI);
-
-    sm_config_set_in_shift(
-        &c,
-        false, // Shift-to-right = false (i.e. shift to left)
-        true,  // Autopush enabled
-        8      // Autopush threshold = 8
-    );
-
-    pio_sm_init(pio, *sm, *offset, &c);
-
-    pio_set_irq0_source_enabled(pio, pis_interrupt0, true);
-	irq_set_exclusive_handler(PIO0_IRQ_0, prepare_data_handler);
-	irq_set_enabled(PIO0_IRQ_0, true);
-}
-
-static void configure_read_dma(PIO pio, uint sm, uint* channel) {
-    uint dma_channel = dma_claim_unused_channel(true);
-
-    dma_channel_config channel_config = dma_channel_get_default_config(dma_channel);
-    channel_config_set_transfer_data_size(&channel_config, DMA_SIZE_8);
-    channel_config_set_dreq(&channel_config, pio_get_dreq(pio, sm, false));
-    //channel_config_set_irq_quiet(&channel_config, true);
-    channel_config_set_read_increment(&channel_config, false);
-    channel_config_set_write_increment(&channel_config, true);
-
-    dma_channel_configure(
-        dma_channel,
-        &channel_config,
-        NULL,    		// dst
-        &pio->rxf[sm], 	 // src
-        1,  					// transfer count
-        false
-    );
-
-    *channel = dma_channel;
-}
-
-static void configure_write_dma(PIO pio, uint sm, uint* channel) {
-    uint dma_channel = dma_claim_unused_channel(true);
-
-    dma_channel_config channel_config = dma_channel_get_default_config(dma_channel);
-    channel_config_set_transfer_data_size(&channel_config, DMA_SIZE_8);
-    channel_config_set_dreq(&channel_config, pio_get_dreq(pio, sm, true));
-    //channel_config_set_irq_quiet(&channel_config, true);
-    channel_config_set_read_increment(&channel_config, true);
-    channel_config_set_write_increment(&channel_config, false);
-
-    dma_channel_configure(
-        dma_channel,
-        &channel_config,
-        &pio->txf[sm],    		// dst
-        NULL, 	 // src
-        1,  					// transfer count
-        false
-    );
-
-    *channel = dma_channel;
-}
-
-typedef void(*transaction_started_func)(void* ctx);
-typedef volatile uint8_t*(*data_request_function)(void* ctx, uint reg, uint* length);
-typedef void(*transaction_ended_func)(void* ctx, const uint8_t* buffer, uint buffer_len);
-
-typedef struct stretched_spi_t {
-    PIO pio;
-    uint sm_write;
-    uint offset_write;
-    uint channel_write;
-    uint sm_read;
-    uint offset_read;
-    uint channel_read;
-    void* context;
-    transaction_started_func transaction_started;
-    data_request_function data_request;
-    transaction_ended_func transaction_ended;
-    volatile uint8_t read_buffer[256];
-} stretched_spi_t;
-
-static stretched_spi_t stretched_spi;
-
-static void __time_critical_func(cs_change_irq)(uint gpio, uint32_t events) {
-    if (gpio != PIN_CS) return;
-
-    if (events & GPIO_IRQ_EDGE_FALL) {
-        gpio_put(PIN_DBG, true);
-        gpio_set_dir(PIN_MISO, GPIO_OUT); // Set MISO to output
-        gpio_put(PIN_MISO, false);
-        pio_sm_exec(stretched_spi.pio, stretched_spi.sm_write, pio_encode_jmp(stretched_spi.offset_write));
-        pio_sm_exec(stretched_spi.pio, stretched_spi.sm_read, pio_encode_jmp(stretched_spi.offset_read));
-        pio_enable_sm_mask_in_sync(stretched_spi.pio, (1u << stretched_spi.sm_write) | (1u << stretched_spi.sm_read));
-
-        dma_channel_abort(stretched_spi.channel_read);
-        dma_channel_transfer_to_buffer_now(stretched_spi.channel_read, stretched_spi.read_buffer, 256);
-
-        if (stretched_spi.transaction_started) stretched_spi.transaction_started(stretched_spi.context);
-
-        gpio_put(PIN_DBG, false);
-    } else if (events & GPIO_IRQ_EDGE_RISE) {
-        gpio_set_dir(PIN_MISO, false);
-        pio_sm_set_enabled(stretched_spi.pio, stretched_spi.sm_read, false);
-        pio_sm_set_enabled(stretched_spi.pio, stretched_spi.sm_write, false);
-        pio_sm_restart(stretched_spi.pio, stretched_spi.sm_read);
-        pio_sm_restart(stretched_spi.pio, stretched_spi.sm_write);
-        pio_interrupt_clear(stretched_spi.pio, 0);
-        pio_interrupt_clear(stretched_spi.pio, 7);
-        pio_sm_clear_fifos(stretched_spi.pio, stretched_spi.sm_read);
-        pio_sm_clear_fifos(stretched_spi.pio, stretched_spi.sm_write);
-
-        if (stretched_spi.transaction_ended) stretched_spi.transaction_ended(stretched_spi.context, (const uint8_t*)stretched_spi.read_buffer, 256);
-    }
-}
-
-void __time_critical_func(data_request_isr)(void) {
-    gpio_put(PIN_DBG, true);
-    uint8_t reg = stretched_spi.read_buffer[0];
-    pio_interrupt_clear(stretched_spi.pio, 0);
-    dma_channel_abort(stretched_spi.channel_write);
-    if (stretched_spi.data_request) {
-        uint buf_len = 0;
-        volatile uint8_t* buf = stretched_spi.data_request(stretched_spi.context, reg, &buf_len);
-        if (buf_len > 0) {
-            dma_channel_transfer_from_buffer_now(stretched_spi.channel_write, buf, buf_len);
-        }
-    }
-    gpio_put(PIN_DBG, false);
-}
 
 static void __time_critical_func(transaction_started)(void* ctx) {
     (void)ctx;
@@ -178,19 +30,30 @@ spin_lock_t* spin_lock = NULL;
 
 extern unsigned int* get_current_values();
 
-static volatile uint8_t* __time_critical_func(data_request)(void* ctx, uint reg, uint* length) {
+static volatile uint8_t* __time_critical_func(data_request)(void* ctx, uint32_t reg, uint32_t* length) {
+
+
     (void)ctx;
     (void)reg;
     unsigned int* buf = get_current_values();
-    *length = 4;
+    *length = 8;
     (void)buf;
     return (volatile uint8_t*)ret_data;
 }
 
-static void __time_critical_func(transaction_ended)(void* ctx, const uint8_t* buffer, uint buffer_len) {
+static void __time_critical_func(transaction_ended)(void* ctx, const uint8_t* buffer, uint32_t buffer_len) {
     (void)ctx;
     (void)buffer;
     (void)buffer_len;
+    SEGGER_RTT_printf(0, "%x %x %x %x %x %x %x %x\n",
+    buffer[0],
+    buffer[1],
+    buffer[2],
+    buffer[3],
+    buffer[4],
+    buffer[5],
+    buffer[6],
+    buffer[7]);
 }
 
 extern void core1_main(void);
@@ -205,36 +68,22 @@ int main()
     multicore_reset_core1();
     multicore_launch_core1(core1_main);
 
-    gpio_init(PIN_CS);
-    gpio_init(PIN_SCK);
-    gpio_init(PIN_MOSI);
-    gpio_init(PIN_MISO);
-    gpio_init(PIN_DBG);
+    stretched_spi_config_t config = {
+        .callback_ctx = NULL,
+        .transaction_ended = transaction_ended,
+        .transaction_started = transaction_started,
+        .data_request = data_request,
+        .cipo_pin = PIN_CIPO,
+        .copi_pin = PIN_COPI,
+        .cs_pin = PIN_CS,
+        .dbg_pin = PIN_DBG,
+        .sck_pin = PIN_SCK,
+        .pio_idx = 0,
+    };
 
-    gpio_set_dir(PIN_CS, GPIO_IN);
-    gpio_set_dir(PIN_SCK, GPIO_IN);
-    gpio_set_dir(PIN_MOSI, GPIO_IN);
-    gpio_set_dir(PIN_MISO, GPIO_OUT);
-    gpio_set_pulls(PIN_CS, false, false);
-    gpio_set_pulls(PIN_SCK, false, false);
-    gpio_set_pulls(PIN_MOSI, false, false);
-    gpio_set_pulls(PIN_MISO, false, false);
-    gpio_set_dir(PIN_DBG, GPIO_OUT);
-    gpio_set_pulls(PIN_DBG, false, false);
-
-    stretched_spi.pio = pio0;
-    stretched_spi.context = NULL;
-    stretched_spi.transaction_started = transaction_started;
-    stretched_spi.transaction_ended = transaction_ended;
-    stretched_spi.data_request = data_request;
-
-    setup_write_sm(stretched_spi.pio, &stretched_spi.sm_write, &stretched_spi.offset_write);
-    configure_write_dma(stretched_spi.pio, stretched_spi.sm_write, &stretched_spi.channel_write);
-
-    setup_read_sm(stretched_spi.pio, &stretched_spi.sm_read, &stretched_spi.offset_read, data_request_isr);
-    configure_read_dma(stretched_spi.pio, stretched_spi.sm_read, &stretched_spi.channel_read);
-
-    gpio_set_irq_enabled_with_callback(PIN_CS, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, cs_change_irq);
+    const stretched_spi_t* spi = stretched_spi_init(&config);
+    (void)spi;
+    //stretched_spi_start(spi);
 
     // Run a heartbeat
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
---
+++ src/stretched_spi.c
@@ -0,0 +1,255 @@
+#include "stretched_spi.h"
+#include "stretched_spi.pio.h"
+#include "hardware/pio.h"
+#include "hardware/gpio.h"
+#include "hardware/dma.h"
+#include "SEGGER_RTT.h"
+
+typedef struct stretched_spi_t {
+    bool allocated;
+    PIO pio;
+    uint sm_write;
+    uint offset_write;
+    uint channel_write;
+    uint sm_read;
+    uint offset_read;
+    uint channel_read;
+    stretched_spi_config_t config;
+    volatile uint8_t read_buffer[256];
+} stretched_spi_t;
+
+
+stretched_spi_t stretched_spi[2];
+
+static void setup_write_sm(PIO pio, int cipo_pin, int copi_pin, uint *sm, uint* offset) {
+    *offset = pio_add_program(pio, &spi_write_loop_program);
+    *sm = pio_claim_unused_sm(pio, true);
+    pio_sm_config c = spi_write_loop_program_get_default_config(*offset);
+    sm_config_set_in_pins(&c, copi_pin);
+    sm_config_set_out_pins(&c, cipo_pin, 1);
+    pio_gpio_init(pio, cipo_pin);
+    pio_sm_set_consecutive_pindirs(pio, *sm, cipo_pin, 1, true);
+
+    sm_config_set_out_shift(
+        &c,
+        false, // Shift-to-right = false (i.e. shift to left)
+        true,  // Autopush enabled
+        8      // Autopush threshold = 8
+    );
+    pio_sm_init(pio, *sm, *offset, &c);
+}
+
+static void setup_read_sm(PIO pio, int copi_pin, uint *sm, uint* offset, irq_handler_t prepare_data_handler) {
+    *offset = pio_add_program(pio, &spi_loop_program);
+    *sm = pio_claim_unused_sm(pio, true);
+    pio_sm_config c = spi_loop_program_get_default_config(*offset);
+    sm_config_set_in_pins(&c, copi_pin);
+
+    sm_config_set_in_shift(
+        &c,
+        false, // Shift-to-right = false (i.e. shift to left)
+        true,  // Autopush enabled
+        8      // Autopush threshold = 8
+    );
+
+    pio_sm_init(pio, *sm, *offset, &c);
+
+    pio_set_irq0_source_enabled(pio, pis_interrupt0, true);
+	irq_set_exclusive_handler(PIO0_IRQ_0, prepare_data_handler);
+	irq_set_enabled(PIO0_IRQ_0, true);
+}
+
+static void configure_read_dma(PIO pio, uint sm, uint* channel) {
+    uint dma_channel = dma_claim_unused_channel(true);
+
+    dma_channel_config channel_config = dma_channel_get_default_config(dma_channel);
+    channel_config_set_transfer_data_size(&channel_config, DMA_SIZE_8);
+    channel_config_set_dreq(&channel_config, pio_get_dreq(pio, sm, false));
+    //channel_config_set_irq_quiet(&channel_config, true);
+    channel_config_set_read_increment(&channel_config, false);
+    channel_config_set_write_increment(&channel_config, true);
+
+    dma_channel_configure(
+        dma_channel,
+        &channel_config,
+        NULL,    		// dst
+        &pio->rxf[sm], 	 // src
+        1,  					// transfer count
+        false
+    );
+
+    *channel = dma_channel;
+}
+
+static void configure_write_dma(PIO pio, uint sm, uint* channel) {
+    uint dma_channel = dma_claim_unused_channel(true);
+
+    dma_channel_config channel_config = dma_channel_get_default_config(dma_channel);
+    channel_config_set_transfer_data_size(&channel_config, DMA_SIZE_8);
+    channel_config_set_dreq(&channel_config, pio_get_dreq(pio, sm, true));
+    //channel_config_set_irq_quiet(&channel_config, true);
+    channel_config_set_read_increment(&channel_config, true);
+    channel_config_set_write_increment(&channel_config, false);
+
+    dma_channel_configure(
+        dma_channel,
+        &channel_config,
+        &pio->txf[sm],    		// dst
+        NULL, 	 // src
+        1,  					// transfer count
+        false
+    );
+
+    *channel = dma_channel;
+}
+
+static void __time_critical_func(cs_change_irq)(uint gpio, uint32_t events, stretched_spi_t* spi) {
+    if (gpio != (uint)spi->config.cs_pin) return;
+
+    if (events & GPIO_IRQ_EDGE_FALL) {
+        gpio_put(spi->config.dbg_pin, true);
+        gpio_set_dir(spi->config.cipo_pin, GPIO_OUT); // Set CIPO to output
+        gpio_put(spi->config.cipo_pin, false);
+        pio_sm_exec(spi->pio, spi->sm_write, pio_encode_jmp(spi->offset_write));
+        pio_sm_exec(spi->pio, spi->sm_read, pio_encode_jmp(spi->offset_read));
+        pio_enable_sm_mask_in_sync(spi->pio, (1u << spi->sm_write) | (1u << spi->sm_read));
+
+        dma_channel_abort(spi->channel_read);
+        dma_channel_transfer_to_buffer_now(spi->channel_read, spi->read_buffer, 256);
+
+        if (spi->config.transaction_started) spi->config.transaction_started(spi->config.callback_ctx);
+
+        gpio_put(spi->config.dbg_pin, false);
+    } else if (events & GPIO_IRQ_EDGE_RISE) {
+        gpio_put(spi->config.cipo_pin, false);
+        gpio_set_dir(spi->config.cipo_pin, false);
+        pio_sm_set_enabled(spi->pio, spi->sm_read, false);
+        pio_sm_set_enabled(spi->pio, spi->sm_write, false);
+        pio_sm_restart(spi->pio, spi->sm_read);
+        pio_sm_restart(spi->pio, spi->sm_write);
+        pio_interrupt_clear(spi->pio, 0);
+        pio_interrupt_clear(spi->pio, 7);
+        pio_sm_clear_fifos(spi->pio, spi->sm_read);
+        pio_sm_clear_fifos(spi->pio, spi->sm_write);
+
+        if (spi->config.transaction_ended) spi->config.transaction_ended(spi->config.callback_ctx, (const uint8_t*)spi->read_buffer, 256);
+    }
+}
+
+static void __time_critical_func(cs_change_irq_0)(uint gpio, uint32_t events) {
+    cs_change_irq(gpio, events, &stretched_spi[0]);
+}
+
+static void __time_critical_func(cs_change_irq_1)(uint gpio, uint32_t events) {
+    cs_change_irq(gpio, events, &stretched_spi[1]);
+}
+
+static void __time_critical_func(data_request_isr)(stretched_spi_t* spi) {
+    gpio_put(spi->config.dbg_pin, true);
+    uint8_t reg = spi->read_buffer[0];
+    pio_interrupt_clear(spi->pio, 0);
+    dma_channel_abort(spi->channel_write);
+    if (spi->config.data_request) {
+        uint32_t buf_len = 0;
+        volatile uint8_t* buf = spi->config.data_request(spi->config.callback_ctx, reg, &buf_len);
+        if (buf_len > 0) {
+            dma_channel_transfer_from_buffer_now(spi->channel_write, buf, buf_len);
+        }
+    }
+    gpio_put(spi->config.dbg_pin, false);
+}
+
+static void __time_critical_func(data_request_isr_0)(void) {
+    data_request_isr(&stretched_spi[0]);
+}
+
+static void __time_critical_func(data_request_isr_1)(void) {
+    data_request_isr(&stretched_spi[1]);
+}
+
+const stretched_spi_t* stretched_spi_init(const stretched_spi_config_t* config) {
+    assert(config);
+    assert(config->pio_idx == 0 || config->pio_idx == 1);
+    stretched_spi_t* spi = &stretched_spi[config->pio_idx];
+    assert(!spi->allocated);
+    spi->allocated = true;
+
+    spi->config = *config;
+    spi->pio = config->pio_idx == 0 ? pio0 : pio1;
+
+    gpio_init(config->dbg_pin);
+    gpio_set_dir(config->dbg_pin, GPIO_OUT);
+    gpio_set_pulls(config->dbg_pin, false, false);
+
+    gpio_init(config->cs_pin);
+    gpio_init(config->sck_pin);
+    gpio_init(config->copi_pin);
+    gpio_init(config->cipo_pin);
+
+    gpio_set_dir(config->cs_pin, GPIO_IN);
+    gpio_set_dir(config->sck_pin, GPIO_IN);
+    gpio_set_dir(config->copi_pin, GPIO_IN);
+    gpio_set_dir(config->cipo_pin, GPIO_IN);
+    gpio_set_pulls(config->cs_pin, false, false);
+    gpio_set_pulls(config->sck_pin, false, false);
+    gpio_set_pulls(config->copi_pin, false, false);
+    gpio_set_pulls(config->cipo_pin, false, false);
+
+    setup_write_sm(spi->pio, spi->config.cipo_pin, spi->config.copi_pin, &spi->sm_write, &spi->offset_write);
+    configure_write_dma(spi->pio, spi->sm_write, &spi->channel_write);
+
+    setup_read_sm(spi->pio, spi->config.copi_pin, &spi->sm_read, &spi->offset_read, config->pio_idx == 0 ? data_request_isr_0 : data_request_isr_1);
+    configure_read_dma(spi->pio, spi->sm_read, &spi->channel_read);
+
+    gpio_set_irq_enabled_with_callback(config->cs_pin, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, config->pio_idx == 0 ? cs_change_irq_0 : cs_change_irq_1);
+
+    // gpio_set_dir(spi->config.cipo_pin, false);
+    // pio_sm_set_enabled(spi->pio, spi->sm_read, false);
+    // pio_sm_set_enabled(spi->pio, spi->sm_write, false);
+    // pio_sm_restart(spi->pio, spi->sm_read);
+    // pio_sm_restart(spi->pio, spi->sm_write);
+    // pio_interrupt_clear(spi->pio, 0);
+    // pio_interrupt_clear(spi->pio, 7);
+    // pio_sm_clear_fifos(spi->pio, spi->sm_read);
+    // pio_sm_clear_fifos(spi->pio, spi->sm_write);
+
+    return spi;
+}
+
+void stretched_spi_free(const stretched_spi_t* spi) {
+    assert(spi);
+    assert(spi->allocated);
+    assert(spi->config.pio_idx == 0 || spi->config.pio_idx == 1);
+    assert(spi == &stretched_spi[spi->config.pio_idx]);
+    stretched_spi[spi->config.pio_idx].allocated = false;
+}
+
+// void stretched_spi_start(const stretched_spi_t* spi) {
+//     assert(spi);
+//     assert(spi->allocated);
+//     assert(spi->config.pio_idx == 0 || spi->config.pio_idx == 1);
+//     assert(spi == &stretched_spi[spi->config.pio_idx]);
+
+
+//     gpio_set_irq_enabled(spi->config.cs_pin, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
+
+
+// }
+
+// void stretched_spi_stop(const stretched_spi_t* spi) {
+//     assert(spi);
+//     assert(spi->allocated);
+//     assert(spi->config.pio_idx == 0 || spi->config.pio_idx == 1);
+//     assert(spi == &stretched_spi[spi->config.pio_idx]);
+
+//     gpio_set_irq_enabled(spi->config.cs_pin, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, false);
+//     gpio_set_dir(spi->config.cipo_pin, false);
+//     pio_sm_set_enabled(spi->pio, spi->sm_read, false);
+//     pio_sm_set_enabled(spi->pio, spi->sm_write, false);
+//     pio_sm_restart(spi->pio, spi->sm_read);
+//     pio_sm_restart(spi->pio, spi->sm_write);
+//     pio_interrupt_clear(spi->pio, 0);
+//     pio_interrupt_clear(spi->pio, 7);
+//     pio_sm_clear_fifos(spi->pio, spi->sm_read);
+//     pio_sm_clear_fifos(spi->pio, spi->sm_write);
+// }
